79d7d0663ed992573fb13f952b11a5bccac954db
mizutani <mizutani614@gmail.com>
2024-08-25 14:58:06 +0900

    Modify template file

--- runhydro_template.h
+++ runhydro_template.h
@@ -1,26 +1,27 @@
 
 !Gridsize
-       integer, parameter :: numr = 120
-       integer, parameter :: numz = 120
+       integer, parameter :: numr = 258 ! 130
+       integer, parameter :: numz = 258 ! 130
        integer, parameter :: numphi = 1
-       integer, parameter :: hydrophi = 256
-       real, parameter :: eps = 1e-3
+       integer, parameter :: hydrophi = 1 ! 256
+       real, parameter :: eps = 1e-3 ! 1e-3
+       integer, parameter :: numprocess = 8
 
 
 !Polytropic index
-       real, parameter :: np1 = 3.0
+       real, parameter :: np1 = 1.7
        real, parameter :: np2 = 1.7
 
-       real, parameter :: mu1 = 1.3
+       real, parameter :: mu1 = 0.5
        real, parameter :: mu2 = 0.5
 
 !Specify Boundary points A and B for a 2d configuration
-        integer, parameter :: ix = 12
-        integer, parameter :: ax = 120
+        integer, parameter :: ix = 80 ! 12?
+        integer, parameter :: ax = 258 ! 130
         integer, parameter :: ay = 2
 
         integer, parameter :: bx = 2
-        integer, parameter :: by = 120
+        integer, parameter :: by = 200 ! 130
 
 !Not sure what these are
        integer, parameter :: rlwb = 2, rupb = numr - 1
